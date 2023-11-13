b4cba6f672649841c0e31182ecafbc1d7214ba91
km-saifullah <kmsaifullah16@gmail.com>
2023-11-13 08:55:29 +0600

    RGB color to Hex Color conversion done

--- js/script.js
+++ js/script.js
@@ -42,18 +42,21 @@ newDivArr.map(item => {
         let red = Number(splitRgb[0]);
         let green = Number(splitRgb[1]);
         let blue = Number(splitRgb[2]);
-        console.log(red,green,blue);
-        rgbToHex(red);
-        item.innerHTML = rgbColor;
+        console.log('red:',red," ",'green:',green," ",'blue:',blue);
+        let redHexaValue = rgbToHex(red);
+        let greenHexaValue = rgbToHex(green);
+        let blueHexaValue = rgbToHex(blue);
+        let hexaColor = `#${redHexaValue}${greenHexaValue}${blueHexaValue}`
+        console.log('Hexa color:',hexaColor);
+        item.innerHTML = hexaColor;
     })
 });
 
 // convert RGB color to Hexadecimal color
 let rgbToHex = (color) => {
     let wholeNumber = color;
-    let decimalPortion = wholeNumber / 16;
-    console.log(decimalPortion);
-    console.log(decimalPortion % 16 );
+    let hexa = wholeNumber.toString(16);
+    return hexa;
 }
 
 
